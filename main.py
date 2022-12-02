c86e85ceed83371c878bbe14b1a2bc11b99d8e21
zhangkai <2993258983@qq.com>
2022-12-02 13:35:46 +0800

    demo1

--- main.py
+++ main.py
@@ -10,41 +10,35 @@ mylambda=532E-6
 
 
 
-plt.figure()
-I,i_c=fun.Diffraction_Rect(mylambda, 0.4,0.4,20)
-plt.plot(i_c)
-
-# I,i_c=fun.Diffraction_Grating(mylambda,0.06,0.5,10,10)
-# plt.imshow(I,cmap="gray")
-# plt.show()
-
-size=10
-size_rect=0.4
-l=1000
-k = 2 * np.pi / mylambda
-x=np.arange(-size/2,size/2,size/1000)
-xx=np.zeros(1000)
-xx[np.abs(x-0)<size_rect/2]=1
-y=xx
-XX,YY=np.meshgrid(xx,y)
-rect=XX *YY
-
-
-
-omega = fft.fftshift(fft.fftfreq(len(x), 1 / 1000))
-omegaLen = len(omega)
-omegaLenF, omegaLenE = omegaLen * 3 // 8, omegaLen * 5 // 8 + 1
-
-E=np.fft.fftshift(np.fft.fft2(np.fft.fftshift(rect)))
-
-E=E*np.exp(1j*k*l)*np.exp(1j*k*(XX**2+YY*2)/(2*l))/(1j*l*mylambda)
-# E=E[omegaLenF:omegaLenE,omegaLenF:omegaLenE]
-I = np.abs(E ** 2)
-I = (I - np.min(I)) / (np.max(I) - np.min(I))
-
-I = np.uint8(I * 255)
-I=I[omegaLenF:omegaLenE,omegaLenF:omegaLenE]
 # plt.figure()
+
+
+# I,i_c=fun.Diffraction_Rect(mylambda,0.4,0.4,10,1000,400)
+# I1,iic=fun.FFT_Rect(mylambda,0.4,0.4,10,800,)
+# plt.subplot(131)
 # plt.imshow(I,cmap="gray")
-plt.plot(I[126])
+# plt.subplot(132)
+# plt.imshow(I1,cmap="gray")
+# plt.subplot(133)
+# plt.imshow(iic,cmap="gray")
+# plt.show()
+#
+# plt.subplot(121)
+# plt.imshow(I,cmap="gray")
+# plt.subplot(122)
+# plt.imshow(I1,cmap="gray")
+# plt.show()
+a=0.01
+d=0.02
+plt.subplot(131)
+I,n=fun.Diffraction_Grating(mylambda,a,d,int(10//d),10,1000,)
+plt.imshow(I,cmap="gray")
+plt.subplot(132)
+I,n=fun.FFT_Grating(mylambda,a,d,int(10//d),10,1000,2000)
+plt.imshow(I,cmap="gray")
+plt.subplot(133)
+
+plt.imshow(n,cmap="gray")
+
+
 plt.show()
--- test.py
+++ test.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import fft
 
+import Function
+
 # n=0.4
 # x=np.arange(0,600+n,n)
 # y=np.sin(2*np.pi*x/5)
@@ -16,35 +18,35 @@ from scipy import fft
 #
 # plt.plot(fx,Y)
 # plt.show()
-def Rect( x_L, y_L, radius, center=(0, 0)):
-
-    x_L = abs(x_L - center[0]) <= radius
-    y_L = abs(y_L - center[1]) <= radius
-    XX, YY = np.meshgrid(x_L, y_L)
-    return XX & YY
-
-rect_a=0.04
-rect_b=0.04
-size_x=6
-size_y=6
-dp=5E-3
-z0=1000
-mylambda=532E-6
-k=2*np.pi/mylambda
-num1=k*rect_a/(2*z0)
-num2=k*rect_b/(2*z0)
-num0=k/(2*z0)
-xx=np.arange(-size_x/2,size_x/2,dp)
-yy=np.arange(-size_y/2,size_y/2,dp)
-XX,YY=np.meshgrid(xx,yy)
-
-
-
-I = (np.sinc(num1*XX)*np.sinc(num2*YY))**2
-I=(I-np.min(I))/(np.max(I)-np.min(I))
-
-plt.imshow(I,cmap="gray")
-plt.show()
+# def Rect( x_L, y_L, radius, center=(0, 0)):
+#
+#     x_L = abs(x_L - center[0]) <= radius
+#     y_L = abs(y_L - center[1]) <= radius
+#     XX, YY = np.meshgrid(x_L, y_L)
+#     return XX & YY
+#
+# rect_a=0.04
+# rect_b=0.04
+# size_x=6
+# size_y=6
+# dp=5E-3
+# z0=1000
+# mylambda=532E-6
+# k=2*np.pi/mylambda
+# num1=k*rect_a/(2*z0)
+# num2=k*rect_b/(2*z0)
+# num0=k/(2*z0)
+# xx=np.arange(-size_x/2,size_x/2,dp)
+# yy=np.arange(-size_y/2,size_y/2,dp)
+# XX,YY=np.meshgrid(xx,yy)
+#
+#
+#
+# I = (np.sinc(num1*XX)*np.sinc(num2*YY))**2
+# I=(I-np.min(I))/(np.max(I)-np.min(I))
+#
+# plt.imshow(I,cmap="gray")
+# plt.show()
 
 # mylambda=532E-6
 # k=2*np.pi/mylambda
@@ -67,3 +69,69 @@ plt.show()
 # plt.imshow(I,cmap="gray")
 # plt.show()
 
+
+
+size_x=10
+size_y=10
+mylambda=532E-6
+k=2*np.pi/mylambda
+distance=1000
+f=200
+xx=np.arange(-size_x/2,size_x/2,size_x/f)
+yy=np.zeros(f)
+
+a=0.1
+b=0.5
+
+n=size_x/b
+c=b
+
+# yy[np.abs(xx)<a/2]=True
+# yy[np.abs(xx)>a/2]=False
+# yy[np.abs(xx)>(c-a)]=True
+# yy[np.abs(xx)>(c)]=False
+# yy[np.abs(xx) > (2*c - a)] = True
+# yy[np.abs(xx) > (2*c)] = False
+# yy[np.abs(xx) > (3*c - a)] = True
+# yy[np.abs(xx) > (3*c)] = False
+#
+#
+# XX,YY=np.meshgrid(yy,yy)
+
+# plt.figure()
+# plt.imshow(XX)
+# plt.show()
+# E = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(XX)))
+# E = E * np.exp(1j * k * distance) * np.exp(1j * k * (XX ** 2 + YY * 2) / (2 * distance)) / (
+#         1j * distance * mylambda)
+#
+# I = np.abs(E ** 2)
+# I = (I - np.min(I)) / (np.max(I) - np.min(I))
+# I = np.uint8(I * 255)
+# plt.figure()
+# plt.plot(I[100])
+#
+# I,ic= Function.Diffraction_Grating(mylambda,a,b,7,size_x,distance,200)
+# # plt.imshow(I,cmap="gray")
+#
+# plt.plot(ic,"r")
+# plt.show()
+
+
+
+
+circ=Function.Rect((xx+0.5)/0.5)+Function.Rect((xx-0.5)/0.5)
+
+XX,YY=np.meshgrid(circ,circ)
+plt.subplot(121)
+plt.imshow(XX,"gray")
+E = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(XX)))
+# E = E * np.exp(1j * k * distance) * np.exp(1j * k * (XX ** 2 + YY * 2) / (2 * distance)) / (
+#         1j * distance * mylambda)
+
+I = np.abs(E ** 2)
+I = (I - np.min(I)) / (np.max(I) - np.min(I))
+I = np.uint8(I * 255)
+plt.subplot(122)
+plt.imshow(I,"gray")
+plt.show()
